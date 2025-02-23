2cd3ee24215aae61e22325fe3a869537963165af
Nin Vannsan <vannsannin.kh@gmail.com>
2025-02-23 18:43:59 +0700

    Version(23/02/2025)

---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+// let a = prompt("input your value");
+// if (a > 20) {
+//   alert("a is greater than 20");
+// } else if (a < 20) {
+//   alert("a is smaller than 20");
+// } else {
+//   alert("a is equal to 20");
+// }
+// for (var a = 0; a < 5; a++) {
+//   document.write(a + " hello world");
+//   document.write("<br/>");
+// }
+console.log("hello");
